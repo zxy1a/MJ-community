36b19979b51122a3d21f0ea3ea3bae789c1df757
zxy1a <1541848653@qq.com>
2022-05-29 22:33:18 +0800

    session

--- src/main/java/com/majiang/community/controller/AuthorizeController.java
+++ src/main/java/com/majiang/community/controller/AuthorizeController.java
@@ -8,7 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
@@ -26,7 +27,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callBack(@RequestParam(value = "code") String code,
-                           @RequestParam(value = "state") String state) throws IOException {
+                           @RequestParam(value = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -35,7 +37,14 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null){
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:index";
+        } else {
+            //登录失败，重新登录
+            return "redirect:index";
+        }
+
     }
 }
