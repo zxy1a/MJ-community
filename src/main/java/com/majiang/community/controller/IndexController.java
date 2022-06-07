561e4a9470be178eeead419530dde02f128d4dce
zxy1a <1541848653@qq.com>
2022-06-07 20:30:56 +0800

    '分页'

--- src/main/java/com/majiang/community/controller/IndexController.java
+++ src/main/java/com/majiang/community/controller/IndexController.java
@@ -1,19 +1,18 @@
 package com.majiang.community.controller;
 
-import com.majiang.community.dto.QuestionDTO;
-import com.majiang.community.mapper.QuestionMapper;
-import com.majiang.community.mapper.UserMapper;
-import com.majiang.community.model.Question;
+import com.majiang.community.dto.PaginationDTO;
+import com.majiang.community.mapper.UserMapper;;
 import com.majiang.community.model.User;
 import com.majiang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
+
 
 @Controller
 public class IndexController {
@@ -26,7 +25,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(value = "size", defaultValue = "5") Integer size,
+                        @RequestParam(value = "page", defaultValue = "1") Integer page){
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
@@ -40,8 +41,8 @@ public class IndexController {
                 }
             }
 
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page, size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
--- src/main/java/com/majiang/community/mapper/QuestionMapper.java
+++ src/main/java/com/majiang/community/mapper/QuestionMapper.java
@@ -4,6 +4,7 @@ package com.majiang.community.mapper;
 import com.majiang.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +16,9 @@ public interface QuestionMapper {
     @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     void create(Question question);
 
-    @Select("select * from question")
-    List<Question> list();
+    @Select("select * from question limit #{offset}, #{size}")
+    List<Question> list(@Param(value = "offset") Integer offset, @Param(value = "size")Integer size);
+
+    @Select("select count(1) from question")
+    Integer count();
 }
