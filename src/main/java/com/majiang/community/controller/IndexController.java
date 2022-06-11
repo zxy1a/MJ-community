4e3e18558f62544f79fdddb136da260c503b41ab
zxy1a <1541848653@qq.com>
2022-06-11 20:40:47 +0800

    '拦截器'

--- src/main/java/com/majiang/community/controller/IndexController.java
+++ src/main/java/com/majiang/community/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.majiang.community.controller;
 
 import com.majiang.community.dto.PaginationDTO;
 import com.majiang.community.mapper.UserMapper;;
-import com.majiang.community.model.User;
 import com.majiang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 
@@ -28,18 +26,6 @@ public class IndexController {
                         Model model,
                         @RequestParam(value = "size", defaultValue = "5") Integer size,
                         @RequestParam(value = "page", defaultValue = "1") Integer page){
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0)
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
 
         PaginationDTO pagination = questionService.list(page, size);
         model.addAttribute("pagination",pagination);
---
+++ src/main/java/com/majiang/community/interceptor/WebConfig.java
@@ -0,0 +1,40 @@
+package com.majiang.community.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    SessionInterceptor sessionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
+
+    }
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations(
+                    "classpath:/META-INF/resources/webjars/");
+        }
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(
+                    CLASSPATH_RESOURCE_LOCATIONS);
+        }
+    }
+
+
+}
