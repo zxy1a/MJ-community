293103dceab81ac48efb28a4e0009dd79a62c2d8
zxy1a <1541848653@qq.com>
2022-06-12 13:11:55 +0800

    '问题详情页面'

--- src/main/java/com/majiang/community/service/QuestionService.java
+++ src/main/java/com/majiang/community/service/QuestionService.java
@@ -99,4 +99,13 @@ public class QuestionService {
 
         return paginationDTO;
     }
+
+    public QuestionDTO getById(Integer id) {
+        Question question = questionMapper.getById(id);
+        QuestionDTO questionDTO = new QuestionDTO();
+        BeanUtils.copyProperties(question, questionDTO);
+        User user = userMapper.findById(id);
+        questionDTO.setUser(user);
+        return questionDTO;
+    }
 }
