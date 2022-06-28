702a09f4d7f156328bcdfc4ee8767e069e38b810
zxy1a <1541848653@qq.com>
2022-06-28 16:58:53 +0800

    '回复列表'

--- src/main/java/com/majiang/community/controller/CommentController.java
+++ src/main/java/com/majiang/community/controller/CommentController.java
@@ -1,6 +1,6 @@
 package com.majiang.community.controller;
 
-import com.majiang.community.dto.CommentDTO;
+import com.majiang.community.dto.CommentCreateDTO;
 import com.majiang.community.dto.ResultDTO;
 import com.majiang.community.exception.CustomizeErrorCode;
 import com.majiang.community.model.Comment;
@@ -21,16 +21,16 @@ public class CommentController {
 
     @ResponseBody
     @RequestMapping(value = "/comment", method = RequestMethod.POST)
-    public Object post(@RequestBody CommentDTO commentDTO,
+    public Object post(@RequestBody CommentCreateDTO commentCreateDTO,
                        HttpServletRequest request) {
         User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
         }
         Comment comment = new Comment();
-        comment.setParentId(commentDTO.getParentId());
-        comment.setContent(commentDTO.getContent());
-        comment.setType(commentDTO.getType());
+        comment.setParentId(commentCreateDTO.getParentId());
+        comment.setContent(commentCreateDTO.getContent());
+        comment.setType(commentCreateDTO.getType());
         comment.setGmtCreate(System.currentTimeMillis());
         comment.setGmtModified(System.currentTimeMillis());
         comment.setCommentator(user.getId());
