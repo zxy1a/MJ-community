d699b6b10c8937313dbb5a7f3df6e2cdc9104847
zxy1a <1541848653@qq.com>
2022-06-29 21:19:03 +0800

    '优化回复'

--- src/main/java/com/majiang/community/controller/CommentController.java
+++ src/main/java/com/majiang/community/controller/CommentController.java
@@ -6,6 +6,7 @@ import com.majiang.community.exception.CustomizeErrorCode;
 import com.majiang.community.model.Comment;
 import com.majiang.community.model.User;
 import com.majiang.community.service.CommentService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +27,9 @@ public class CommentController {
         User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
+        }
+        if (commentCreateDTO == null || StringUtils.isBlank(commentCreateDTO.getContent())) {
+            return ResultDTO.errorOf(CustomizeErrorCode.CONTENT_IS_EMPTY);
         }
         Comment comment = new Comment();
         comment.setParentId(commentCreateDTO.getParentId());
