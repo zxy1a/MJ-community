b39e5b54127baeec2ec2e5cf22d864bae4cf1d05
zxy1a <1541848653@qq.com>
2022-07-12 11:00:57 +0800

    'tag'

--- src/main/java/com/majiang/community/mapper/QuestionExtMapper.java
+++ src/main/java/com/majiang/community/mapper/QuestionExtMapper.java
@@ -2,7 +2,10 @@ package com.majiang.community.mapper;
 
 import com.majiang.community.model.Question;
 
+import java.util.List;
+
 public interface QuestionExtMapper {
     int incView(Question record);
     int incCommentCount(Question record);
+    List<Question> selectRelated(Question question);
 }
