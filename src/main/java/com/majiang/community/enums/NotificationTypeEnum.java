f54ffa8bb22f72d7d4064a03e73fa7e8a1b6da4e
zxy1a <1541848653@qq.com>
2022-07-13 23:14:03 +0800

    'status'

--- src/main/java/com/majiang/community/enums/NotificationTypeEnum.java
+++ src/main/java/com/majiang/community/enums/NotificationTypeEnum.java
@@ -2,8 +2,7 @@ package com.majiang.community.enums;
 
 public enum NotificationTypeEnum {
     REPLY_QUESTION(1,"回复了问题"),
-    REPLY_COMMENT(1,"回复了评论"),
-    ;
+    REPLY_COMMENT(2,"回复了评论");
     private int type;
     private String name;
 
